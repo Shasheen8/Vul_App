cfd96380a2f97aaba3477eeaec7a178a94a8aca5
Shasheen_B <50681308+Shasheen8@users.noreply.github.com>
2021-06-17 01:57:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,27 @@
+from enum import unique
 from flask import Flask, render_template
 from flask_wtf import Form 
 from wtforms import StringField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Email
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String
-import os
 
 app = Flask(__name__)
 Bootstrap(app)
-app.config['SECRET_KEY'] = 'yoursecretkey'
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'test.db')
+app.config['SECRET_KEY'] = '984af4b0ba1bca3daf8b096db03e17ad'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 
 db = SQLAlchemy(app)
 
-@app.cli.command('db_create') #database creation
-def db_create():
-	db.create.all()
-	print('Database Created')
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), unique=True, nullable=False)
 
-@app.cli.command('db_create') #database dropped
-def db_drop():
-	db.drop.all()
-	print('Database Dropped')
-
+def __repr__(self):
+	return '<User {}>'.format(self.username)
 
 class NormalForm(Form):
-	username = StringField('Input Field: Username', validators=[InputRequired()])
+	username = StringField('Input Field: Username', validators=[InputRequired(), Email()])
 
 @app.route('/', methods=['GET','POST'])
 def home():
